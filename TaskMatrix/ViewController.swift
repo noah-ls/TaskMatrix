0fe91a3698a76997e7da5db87c990a4e276888c9
noahsli <lis@softin-tech.com>
2026-04-09 18:17:50 +0800

    project initialize

---
+++ TaskMatrix/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TaskMatrix
+//
+//  Created by 123 on 4/9/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
